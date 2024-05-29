c5afb4b1bce836874b46fc33c95cac0946c92666
Bryan Hayes <b.hayes.ortiz@gmail.com>
2024-05-29 14:57:22 -0400

    cambios

---
+++ Add_Two_Numbers.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        pass
+
+# Example usage:
+if __name__ == "__main__":
+    l1 = ListNode(2, ListNode(4, ListNode(3)))
+    l2 = ListNode(5, ListNode(6, ListNode(4)))
+    result = Solution().addTwoNumbers(l1, l2)
+    
+    while result:
+        print(result.val, end=" -> ")
+        result = result.next
+    print("None")
