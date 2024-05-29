aa727a6f97bcd7d14f27818ccea6cd203b1f0c31
Bryan Hayes <b.hayes.ortiz@gmail.com>
2024-05-29 17:42:46 -0400

    Excercise 5

---
+++ Python/5. Longest_Palindromic_Substring.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if s[::-1] == s:
+            return s
+
+        start, size = 0, 1
+
+        for i in range(1, len(s)):
+            l, r = i - size, i + 1
+            s1 = s[l-1:r]
+
+            if l >= 1 and s1[::-1] == s1:
+                size += 2
+                start = l - 1
+            else:
+                s2 = s[l:r]
+                if s2[::-1] == s2:
+                    size += 1
+                    start = l
+            
+        return s[start:start+size]
+
+if __name__ == "__main__":
+    s = "aabaabbabababaabababa"
+    result = Solution().longestPalindrome(s)
+    print(result)
